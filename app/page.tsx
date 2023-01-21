fc9c737f9bd999e806b828d963623f28c3d71291
Musa Jundi <musajundi@gmail.com>
2023-01-21 02:37:47 -0600

    Add visual layout of landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,31 @@
+import Image from "next/image";
+import styles from "./styles.module.css"
+import { Oxygen } from "@next/font/google";
+
+const oxygen = Oxygen({
+    weight: '700',
+    subsets: ['latin'],
+    
+})
+
 export default function LandingPage() {
     return (
-        <div>
-            <h1>Landing Page</h1>
-            <p>Some content</p>
+        <div className={styles.container}>
+            <Image 
+                className={styles.landingImage}
+                src="/images/crescent-moon.jpg"
+                alt="crescent-moon"
+                fill={true}
+            />
+            <div className={styles.content}>
+                <h1 className={oxygen.className}>Lunar Terminator Visualization Tool</h1>
+                <button className={styles.launchBtn}>Open App</button>
+                <div className={styles.linkBtnContainer}>
+                    <button>Button One</button>
+                    <button>Button Two</button>
+                    <button>Button Three</button>
+                </div>
+            </div>
         </div>
     );
 }
